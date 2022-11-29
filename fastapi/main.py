75cbb0245c1ed55274f19c6b2efd88c4b99f35c4
Oleg1202000 <oleg1202000@gmail.com>
2022-11-29 11:49:04 +0300

    Path Parameters: add /users

--- fastapi/main.py
+++ fastapi/main.py
@@ -13,3 +13,12 @@ async def root():
 async def read_item(item_id: float):
     return {'item_id': item_id}
 
+
+@app.get('/users/me')
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get('/users/{user_id}')
+async def read_user_me(user_id: str):
+    return {"user_id": user_id}

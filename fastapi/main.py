c404975ab2961bb7cb90ced4a6a8e966587a76a0
Oleg1202000 <oleg1202000@gmail.com>
2022-11-29 11:58:11 +0300

    Path Parameters: add /models

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,4 +1,12 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 app = FastAPI()
@@ -22,3 +30,14 @@ async def read_user_me():
 @app.get('/users/{user_id}')
 async def read_user_me(user_id: str):
     return {"user_id": user_id}
+
+
+@app.get('/models/{model_name}')
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}

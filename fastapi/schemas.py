d6173abdd1d8770f0066cbbbe055a3a85e61d076
Oleg1202000 <oleg1202000@gmail.com>
2022-11-30 00:24:06 +0300

    Body - Nested Models

--- fastapi/schemas.py
+++ fastapi/schemas.py
@@ -2,12 +2,19 @@ from datetime import date
 from pydantic import BaseModel, Field
 
 
+class Image(BaseModel):
+    url: str
+    name: str
+
+
 class Item(BaseModel):
     name: str
     description: str | None = Field(default=None, max_length=20)
     price: float
     tax: float | None = None
     date: date
+    tag: set[str] = set()
+    image: Image | None = None
 
 
 class User(BaseModel):
